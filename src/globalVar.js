3ea5aad09c0c1767d0205325a9e46ccf5fadd828
spemer <ghsspower@naver.com>
2018-11-16 17:46:23 +0900

    .

--- src/globalVar.js
+++ src/globalVar.js
@@ -9,8 +9,8 @@ export const globalVar = new Vue ({
     companyCEO: 'JOHNNY HAM ILMAN',
     companyAdr: '대구광역시 동구 동대구로 471 크리에이티브팩토리 401호',
 
-    callTo: '02-0000-0000',
-    mailTo: 'privacy@tite.com',
+    callTo: '1544-7958',
+    mailTo: 'corporate@tite.com',
 
     serviceKo: '로라',
     serviceKoFull: '로컬라이프',
