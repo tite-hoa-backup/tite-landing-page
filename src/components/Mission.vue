91c4606958a96495f4fbbc97e9b2cc504f0a6658
spemer <ghsspower@naver.com>
2018-11-16 17:34:04 +0900

    .

---
+++ src/components/Mission.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div#mission
+</template>
+
+<script>
+export default {
+  name: 'mission',
+
+  data () {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
