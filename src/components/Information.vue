0b4fc11028491b7d34545944c67ca77d1f6bcefa
spemer <ghsspower@naver.com>
2018-11-26 04:05:59 +0900

    FIX 지역-로컬

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -5,7 +5,7 @@
         h1.information__text--title
           | {{ $t('generic.title') }}
         h3.information__text--subtitle
-          | {{ serviceKo }}는 나와 나의 이웃을들 위한 폐쇄형 지역 네트워크입니다.
+          | {{ serviceKo }}는 나와 나의 이웃을들 위한 폐쇄형 로컬 네트워크입니다.
           <br>
           | 집이라고 부를수 있는 더 행복하고, 더 안전한곳을 만들기 위해 이미 많은 이웃들이 로라를 사용중입니다.
 </template>
