d502e85111b1f826a3c4fd2b819802bb33eb4f16
spemer <ghsspower@naver.com>
2018-11-27 04:59:06 +0900

    .

--- src/locale/ko.js
+++ src/locale/ko.js
@@ -3,7 +3,7 @@ export default {
 		'close': "창 닫기",
     'title': "로컬 라이프",
     'company': "타이트",
-		'tagline': "진짜 이웃들의 소셜 커뮤니티",
+		'tagline': "진짜 이웃들의 로컬 커뮤니티",
 		'downloadHeader': "어플로 간편하게 즐겨보세요",
     'download': "어플다운받기",
     'serviceTitle': "이웃과 함께하세요",
