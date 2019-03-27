016c33b3712dcb707dc57a8c4705f75fc1c14c11
spemer <ghsspower@gmail.com>
2019-03-28 05:15:55 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
 
 echo "============================================================"
 echo "${BOLD}${PWD##*/}${RESET}"
@@ -12,7 +14,7 @@ echo "============================================================"
 image_resizer() {
   while true; do
     printf "\n"
-    read -p "${BOLD}Run gulpfile.js(image resizer)? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}Run gulpfile.js(image resizer)? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* )
         trap 'echo Stop gulp-watch' SIGINT
@@ -32,7 +34,7 @@ image_resizer() {
 npm_run_dev() {
   while true; do
     printf "\n"
-    read -p "${BOLD}npm run dev? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}npm run dev? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) npm run dev; break;;
       [Nn]* ) return 0;;
@@ -47,7 +49,7 @@ npm_run_dev() {
 npm_run_build() {
   while true; do
     printf "\n"
-    read -p "${BOLD}npm run build? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}npm run build? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) npm run build; break;;
       [Nn]* ) return 0;;
@@ -62,7 +64,7 @@ npm_run_build() {
 firebase_deploy() {
   while true; do
     printf "\n"
-    read -p "${BOLD}firebase deploy? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}firebase deploy? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) firebase deploy; break;;
       [Nn]* ) return 0;;
@@ -77,7 +79,7 @@ firebase_deploy() {
 git_commit() {
   while true; do
     printf "\n"
-    read -p "${BOLD}git commit? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}git commit? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* )
         IFS= read -r -p "${BOLD}Enter commit message: ${RESET}" commitmsg
@@ -85,7 +87,7 @@ git_commit() {
         # if commitmsg empty
         if [ -z "$commitmsg" ]
         then
-          echo "${BOLD}Commit message is empty${RESET}"
+          echo "${BOLD}${RED}Commit message is empty${RESET}"
           commitmsg="Add files via upload"
         fi
 
@@ -106,7 +108,7 @@ git_commit() {
 git_push() {
   while true; do
     printf "\n"
-    read -p "${BOLD}git push? (Y/n)${RESET}" yn
+    read -p "${BOLD}git push? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) git push; break;;
       [Nn]* ) return 0;;
